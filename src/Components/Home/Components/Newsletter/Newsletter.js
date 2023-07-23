bb584e4b5ec9a4dc182a1d07cec15efb98ae3677
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-07-23 01:56:09 +0100

    added mobile homepage

--- src/Components/Home/Components/Newsletter/Newsletter.js
+++ src/Components/Home/Components/Newsletter/Newsletter.js
@@ -18,9 +18,8 @@ function Newsletter() {
         <div className='subscribe'>
             <p>STAY UP TO DATE</p>
             <h5>Sign up to our Newsletter</h5>
-            <input value={email} onChange={getEmail} type='email' id='email' name='email' placeholder='Enter Email address'/>
             <div className='btn-policy'>
-                <button className='subcribe-btn big-btn yellow-btn' onClick={subscribe}>Subscribe</button>
+                <button className='subscribe-btn big-btn yellow-btn' onClick={subscribe}>Subscribe</button>
                 <p>For information on how we process your data, see our <a href='/home'>Privacy Policy</a></p>
             </div>
         </div>
