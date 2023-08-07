15820631b1d4906fa00aab7429748caf8b12cc1a
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-08-07 02:14:00 +0100

    hash link smooth scrolling

--- src/Components/Policy/Policy.js
+++ src/Components/Policy/Policy.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../Header/Header.js'
 import Footer from '../Footer/Footer'
 import PrivacyPolicy from './PrivacyPolicy/PrivacyPolicy.js'
 import FAQS from './FAQS/FAQS.js'
 
 function Policy() {
+  useEffect(() => {
+    if(window.location.hash) {
+      const element = document.querySelector(window.location.hash)
+      element.scrollIntoView({behavior: 'smooth'})
+    }
+  }, [])
   return (
     <div>
         <Header/>
